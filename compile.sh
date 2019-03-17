827296f7880e596be0c1171f49c5fa7b50b9c2c8
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-03-17 11:12:12 +0000

    genericised it

--- compile.sh
+++ compile.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
+# Try to determine the name of the compiler on this system ...
+if type gfortran-mp-8 &> /dev/null; then
+    fc=gfortran-mp-8
+elif type gfortran8 &> /dev/null; then
+    fc=gfortran8
+else
+    fc=gfortran
+fi
+
+# Clean up ...
 rm -f *.mod *.o
-gfortran8 -c -ffree-line-length-none mod_safe.F90
+
+# Compile ...
+$fc -c -ffree-line-length-none mod_safe.F90

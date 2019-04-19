98bdef17cd4fc86ec34b5a0fc4f33c835569394d
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-04-19 11:34:57 +0100

    now doesn't try to be clever about determining the compiler name

--- compile.sh
+++ compile.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# Try to determine the name of the compilers on this system ...
-if type gfortran-mp-8 &> /dev/null; then
-    fc1=gfortran-mp-8
-elif type gfortran8 &> /dev/null; then
-    fc1=gfortran8
-else
-    fc1=gfortran
-fi
-fc2=mpif90
-
 # Clean up ...
 rm -f *.mod *.o
 
@@ -18,5 +8,5 @@ LANG_OPTS="-ffree-form -ffree-line-length-none"
 WARN_OPTS="-Wall -Wextra -Waliasing -Warray-temporaries -Wconversion-extra -Wimplicit-interface -Wimplicit-procedure"
 
 # Compile ...
-$fc1 -c ${LANG_OPTS} ${WARN_OPTS} mod_safe.F90
-$fc2 -c ${LANG_OPTS} ${WARN_OPTS} mod_safe_mpi.F90
+mpif90 -c ${LANG_OPTS} ${WARN_OPTS} mod_safe.F90
+mpif90 -c ${LANG_OPTS} ${WARN_OPTS} mod_safe_mpi.F90
--- tests/compile.sh
+++ tests/compile.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# Try to determine the name of the compilers on this system ...
-if type gfortran-mp-8 &> /dev/null; then
-    fc1=gfortran-mp-8
-elif type gfortran8 &> /dev/null; then
-    fc1=gfortran8
-else
-    fc1=gfortran
-fi
-fc2=mpif90
-
 # Clean up ...
 rm -f *.mod *.o
 
@@ -18,9 +8,9 @@ LANG_OPTS="-ffree-form -ffree-line-length-none"
 WARN_OPTS="-Wall -Wextra -Waliasing -Warray-temporaries -Wconversion-extra -Wimplicit-interface -Wimplicit-procedure"
 
 # Compile ...
-$fc2 -c ${LANG_OPTS} ${WARN_OPTS} -I../ test01.F90
-$fc2 -c ${LANG_OPTS} ${WARN_OPTS} -I../ test02.F90
-$fc2 -c ${LANG_OPTS} ${WARN_OPTS} -I../ test03.F90
-$fc2 -o test01 ${LANG_OPTS} ${WARN_OPTS} test01.o ../*.o -L../
-$fc2 -o test02 ${LANG_OPTS} ${WARN_OPTS} test02.o ../*.o -L../
-$fc2 -o test03 ${LANG_OPTS} ${WARN_OPTS} test03.o ../*.o -L../
+mpif90 -c ${LANG_OPTS} ${WARN_OPTS} -I../ test01.F90
+mpif90 -c ${LANG_OPTS} ${WARN_OPTS} -I../ test02.F90
+mpif90 -c ${LANG_OPTS} ${WARN_OPTS} -I../ test03.F90
+mpif90 -o test01 ${LANG_OPTS} ${WARN_OPTS} test01.o ../*.o -L../
+mpif90 -o test02 ${LANG_OPTS} ${WARN_OPTS} test02.o ../*.o -L../
+mpif90 -o test03 ${LANG_OPTS} ${WARN_OPTS} test03.o ../*.o -L../

f6d605cae8913a6ccf635485820cc15456669486
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-04-21 08:16:34 +0100

    renamed variables to make it neater

--- mod_safe/func_integrate_array/func_integrate_1D_REAL64_real_array.f90
+++ mod_safe/func_integrate_array/func_integrate_1D_REAL64_real_array.f90
@@ -1,49 +1,49 @@
 !> @brief This function returns the integral of the supplied 1D tabulated data.
 !>
-!> @param[in] x The variable of integration.
+!> @param[in] x1 The variable of integration.
 !>
 !> @param[in] array The integrand.
 !>
-!> @note "x" does not have to be uniform.
+!> @note "x1" does not have to be uniform.
 !>
-!> @warning "x" must be the same dimension as "array".
+!> @warning "x1" must be the same dimension as "array".
 !>
-FUNCTION func_integrate_1D_REAL64_real_array(x, array) RESULT(ans)
+FUNCTION func_integrate_1D_REAL64_real_array(x1, array) RESULT(ans)
     USE ISO_FORTRAN_ENV
     USE OMP_LIB
 
     IMPLICIT NONE
 
     ! Declare input variables ...
-    REAL(kind = REAL64), DIMENSION(:), INTENT(in)                               :: x
+    REAL(kind = REAL64), DIMENSION(:), INTENT(in)                               :: x1
     REAL(kind = REAL64), DIMENSION(:), INTENT(in)                               :: array
 
     ! Declare output variables ...
     REAL(kind = REAL64)                                                         :: ans
 
     ! Declare internal variables ...
-    INTEGER(kind = INT64)                                                       :: i_x
-    INTEGER(kind = INT64)                                                       :: n_x
+    INTEGER(kind = INT64)                                                       :: i1
+    INTEGER(kind = INT64)                                                       :: n1
 
     ! Find size of input array ...
-    n_x = SIZE(x, kind = INT64)
+    n1 = SIZE(x1, kind = INT64)
 
     ! Set starting value ...
     ans = 0.0e0_REAL64
 
     !$omp parallel                                                              &
     !$omp default(none)                                                         &
-    !$omp private(i_x)                                                          &
-    !$omp shared(n_x)                                                           &
-    !$omp shared(x)                                                             &
+    !$omp private(i1)                                                           &
+    !$omp shared(n1)                                                            &
+    !$omp shared(x1)                                                            &
     !$omp shared(array)                                                         &
     !$omp reduction(+:ans)
         !$omp do &
         !$omp schedule(dynamic)
             ! Loop over x ...
-            DO i_x = 1_INT64, n_x - 1_INT64
+            DO i1 = 1_INT64, n1 - 1_INT64
                 ! Integrate via the trapezium rule ...
-                ans = ans + (x(i_x + 1_INT64) - x(i_x)) * 0.5e0_REAL64 * (array(i_x) + array(i_x + 1_INT64))
+                ans = ans + (x1(i1 + 1_INT64) - x1(i1)) * 0.5e0_REAL64 * (array(i1) + array(i1 + 1_INT64))
             END DO
         !$omp end do
     !$omp end parallel
--- mod_safe/func_integrate_array/func_integrate_2D_REAL64_real_array.f90
+++ mod_safe/func_integrate_array/func_integrate_2D_REAL64_real_array.f90
@@ -1,62 +1,62 @@
 !> @brief This function returns the integral of the supplied 2D tabulated data.
 !>
-!> @param[in] x The first variable of integration.
+!> @param[in] x1 The first variable of integration.
 !>
-!> @param[in] y The second variable of integration.
+!> @param[in] x2 The second variable of integration.
 !>
 !> @param[in] array The integrand.
 !>
-!> @note "x" and "y" do not have to be uniform.
+!> @note "x1" and "x2" do not have to be uniform.
 !>
-!> @warning "x" must be the same as the first dimension of "array".
+!> @warning "x1" must be the same as the first dimension of "array".
 !>
-!> @warning "y" must be the same as the second dimension of "array".
+!> @warning "x2" must be the same as the second dimension of "array".
 !>
-FUNCTION func_integrate_2D_REAL64_real_array(x, y, array) RESULT(ans)
+FUNCTION func_integrate_2D_REAL64_real_array(x1, x2, array) RESULT(ans)
     USE ISO_FORTRAN_ENV
     USE OMP_LIB
 
     IMPLICIT NONE
 
     ! Declare input variables ...
-    REAL(kind = REAL64), DIMENSION(:), INTENT(in)                               :: x
-    REAL(kind = REAL64), DIMENSION(:), INTENT(in)                               :: y
+    REAL(kind = REAL64), DIMENSION(:), INTENT(in)                               :: x1
+    REAL(kind = REAL64), DIMENSION(:), INTENT(in)                               :: x2
     REAL(kind = REAL64), DIMENSION(:, :), INTENT(in)                            :: array
 
     ! Declare output variables ...
     REAL(kind = REAL64)                                                         :: ans
 
     ! Declare internal variables ...
-    INTEGER(kind = INT64)                                                       :: i_x
-    INTEGER(kind = INT64)                                                       :: i_y
-    INTEGER(kind = INT64)                                                       :: n_x
-    INTEGER(kind = INT64)                                                       :: n_y
+    INTEGER(kind = INT64)                                                       :: i1
+    INTEGER(kind = INT64)                                                       :: i2
+    INTEGER(kind = INT64)                                                       :: n1
+    INTEGER(kind = INT64)                                                       :: n2
 
     ! Find size of input arrays ...
-    n_x = SIZE(x, kind = INT64)
-    n_y = SIZE(y, kind = INT64)
+    n1 = SIZE(x1, kind = INT64)
+    n2 = SIZE(x2, kind = INT64)
 
     ! Set starting value ...
     ans = 0.0e0_REAL64
 
     !$omp parallel                                                              &
     !$omp default(none)                                                         &
-    !$omp private(i_x)                                                          &
-    !$omp private(i_y)                                                          &
+    !$omp private(i1)                                                           &
+    !$omp private(i2)                                                           &
     !$omp shared(array)                                                         &
-    !$omp shared(n_x)                                                           &
-    !$omp shared(n_y)                                                           &
-    !$omp shared(x)                                                             &
-    !$omp shared(y)                                                             &
+    !$omp shared(n1)                                                            &
+    !$omp shared(n2)                                                            &
+    !$omp shared(x1)                                                            &
+    !$omp shared(x2)                                                            &
     !$omp reduction(+:ans)
         !$omp do &
         !$omp schedule(dynamic)
             ! Loop over x ...
-            DO i_x = 1_INT64, n_x - 1_INT64
+            DO i1 = 1_INT64, n1 - 1_INT64
                 ! Loop over y ...
-                DO i_y = 1_INT64, n_y - 1_INT64
+                DO i2 = 1_INT64, n2 - 1_INT64
                     ! Integrate via the trapezium rule ...
-                    ans = ans + (x(i_x + 1_INT64) - x(i_x)) * (y(i_y + 1_INT64) - y(i_y)) * 0.25e0_REAL64 * (array(i_x, i_y) + array(i_x + 1_INT64, i_y) + array(i_x, i_y + 1_INT64) + array(i_x + 1_INT64, i_y + 1_INT64))
+                    ans = ans + (x1(i1 + 1_INT64) - x1(i1)) * (x2(i2 + 1_INT64) - x2(i2)) * 0.25e0_REAL64 * (array(i1, i2) + array(i1 + 1_INT64, i2) + array(i1, i2 + 1_INT64) + array(i1 + 1_INT64, i2 + 1_INT64))
                 END DO
             END DO
         !$omp end do

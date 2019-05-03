1a2c08dced73208175483e7bf7d5573649543ec8
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-05-03 07:49:13 +0100

    removed OpenMP; added recursion; added 64-bit target machine

--- compile.sh
+++ compile.sh
@@ -4,9 +4,10 @@
 rm -f *.mod *.o
 
 # Define options ...
-LANG_OPTS="-ffree-form -ffree-line-length-none -fopenmp"
+LANG_OPTS="-ffree-form -ffree-line-length-none -frecursive"
 WARN_OPTS="-Wall -Wextra -Waliasing -Warray-temporaries -Wconversion-extra -Wimplicit-interface -Wimplicit-procedure"
+MACH_OPTS="-m64"
 
 # Compile ...
-mpif90 -c ${LANG_OPTS} ${WARN_OPTS} mod_safe.F90
-mpif90 -c ${LANG_OPTS} ${WARN_OPTS} mod_safe_mpi.F90
+mpif90 -c ${LANG_OPTS} ${WARN_OPTS} ${MACH_OPTS} mod_safe.F90
+mpif90 -c ${LANG_OPTS} ${WARN_OPTS} ${MACH_OPTS} mod_safe_mpi.F90
--- tests/compile.sh
+++ tests/compile.sh
@@ -4,12 +4,13 @@
 rm -f *.mod *.o
 
 # Define options ...
-LANG_OPTS="-ffree-form -ffree-line-length-none -fopenmp"
+LANG_OPTS="-ffree-form -ffree-line-length-none -frecursive -fopenmp"
 WARN_OPTS="-Wall -Wextra -Waliasing -Warray-temporaries -Wconversion-extra -Wimplicit-interface -Wimplicit-procedure"
+MACH_OPTS="-m64"
 
 # Compile ...
 for i in {1..6}; do
     name=$(printf test%02d $i)
-    mpif90 -c ${LANG_OPTS} ${WARN_OPTS} -I../ $name.F90
-    mpif90 -o $name ${LANG_OPTS} ${WARN_OPTS} $name.o ../*.o -L../
+    mpif90 -c ${LANG_OPTS} ${WARN_OPTS} ${MACH_OPTS} -I../ $name.F90
+    mpif90 -o $name ${LANG_OPTS} ${WARN_OPTS} ${MACH_OPTS} $name.o ../*.o -L../
 done

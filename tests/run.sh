74724f985028afb6c3071e76e5f0810d6f087ffb
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-11-01 13:03:12 +0000

    added check for programs; consolidated convert/optipng

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+# Check that non-standard programs are installed. "standard" programs are
+# anything that is specified in the POSIX.1-2008 standard (and the IEEE Std
+# 1003.1 standard) or that is a BASH builtin command. Therefore, "non-standard"
+# programs are anything that does not appear on the following two lists:
+#   * http://pubs.opengroup.org/onlinepubs/9699919799/idx/utilities.html
+#   * https://www.gnu.org/software/bash/manual/html_node/Bash-Builtins.html
+if ! type convert &> /dev/null; then
+    echo "ERROR: \"convert\" is not installed." >&2
+    exit 1
+fi
+if ! type exiftool &> /dev/null; then
+    echo "ERROR: \"exiftool\" is not installed." >&2
+    exit 1
+fi
+if ! type mpirun &> /dev/null; then
+    echo "ERROR: \"mpirun\" is not installed." >&2
+    exit 1
+fi
+if ! type optipng &> /dev/null; then
+    echo "ERROR: \"optipng\" is not installed." >&2
+    exit 1
+fi
+
 echo "Running test01 ..."
 mpirun --oversubscribe -np 2 ./test01
 
@@ -26,26 +49,17 @@ echo "Running test08 ..."
 
 echo "Running test09 ..."
 ./test09
-convert test09.pbm test09.png
-optipng test09.png
 
 echo "Running test10 ..."
 ./test10
-convert test10.pgm test10.png
-optipng test10.png
 
 echo "Running test11 ..."
 ./test11
-convert test11_fire.ppm test11_fire.png
-optipng test11_fire.png
-convert test11_jet.ppm test11_jet.png
-optipng test11_jet.png
-convert test11_g2b.ppm test11_g2b.png
-optipng test11_g2b.png
-convert test11_r2g.ppm test11_r2g.png
-optipng test11_r2g.png
-convert test11_r2o2g.ppm test11_r2o2g.png
-optipng test11_r2o2g.png
 
 echo "Running test12 ..."
 ./test12
+
+echo "Converting images ..."
+mogrify -format png *.pbm *.pgm *.ppm &> /dev/null
+exiftool -overwrite_original -all= *.png &> /dev/null
+optipng *.png &> /dev/null

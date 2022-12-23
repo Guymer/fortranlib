cb0a8d5bca4eb33ccefa441be54d510fa7238606
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-12-23 15:01:04 +0000

    use ISO_FORTRAN_ENV instead of ISO_C_BINDING

--- mod_geo.F90
+++ mod_geo.F90
@@ -1,25 +1,47 @@
 MODULE mod_geo
     ! Import standard modules ...
-    USE ISO_C_BINDING
+    USE ISO_FORTRAN_ENV
 
     IMPLICIT NONE
 
-    ! Declare types ...
+    ! Declare type ...
     TYPE aPoint
-        REAL(kind = C_DOUBLE)                                                   :: x    ! longitude
-        REAL(kind = C_DOUBLE)                                                   :: y    ! latitude
+        REAL(kind = REAL64)                                                     :: x    ! longitude
+        REAL(kind = REAL64)                                                     :: y    ! latitude
     END TYPE aPoint
+
+    ! Declare type ...
     TYPE aLine
-        INTEGER(kind = C_SHORT)                                                 :: ring ! 0 = false; 1 = true
-        INTEGER(kind = C_LONG)                                                  :: npoints
-        REAL(kind = C_DOUBLE), ALLOCATABLE, DIMENSION(:)                        :: x    ! longitude
-        REAL(kind = C_DOUBLE), ALLOCATABLE, DIMENSION(:)                        :: y    ! latitude
+        LOGICAL(kind = INT8)                                                    :: ring
+        INTEGER(kind = INT32)                                                   :: npoints
+        REAL(kind = REAL64), ALLOCATABLE, DIMENSION(:)                          :: x    ! longitude
+        REAL(kind = REAL64), ALLOCATABLE, DIMENSION(:)                          :: y    ! latitude
     END TYPE aLine
+
+    ! Declare type ...
     TYPE aPolygon
-        INTEGER(kind = C_SHORT)                                                 :: ninteriors
+        INTEGER(kind = INT32)                                                   :: ninteriors
         TYPE(aLine)                                                             :: exterior
         TYPE(aLine), ALLOCATABLE, DIMENSION(:)                                  :: interiors
     END TYPE aPolygon
+
+    ! Declare type ...
+    TYPE manyPoints
+        INTEGER(kind = INT32)                                                   :: npoints
+        TYPE(aPoint), ALLOCATABLE, DIMENSION(:)                                 :: points
+    END TYPE manyPoints
+
+    ! Declare type ...
+    TYPE manyLines
+        INTEGER(kind = INT32)                                                   :: nlines
+        TYPE(aLine), ALLOCATABLE, DIMENSION(:)                                  :: lines
+    END TYPE manyLines
+
+    ! Declare type ...
+    TYPE manyPolygons
+        INTEGER(kind = INT32)                                                   :: npolygons
+        TYPE(aPolygon), ALLOCATABLE, DIMENSION(:)                               :: polygons
+    END TYPE manyPolygons
 
     ! Declare interfaces ...
     INTERFACE sub_buffer

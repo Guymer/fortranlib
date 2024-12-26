b76ef16fdc516f06061892ef73eb02e18ada42bf
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2024-12-26 11:25:02 +0000

    add subroutines to flood an array

--- mod_safe.F90
+++ mod_safe.F90
@@ -6,7 +6,7 @@ MODULE mod_safe
 
     IMPLICIT NONE
 
-    ! Declare interfaces ...
+    ! Declare interface ...
     INTERFACE func_integrate_array
         MODULE PROCEDURE func_integrate_1D_REAL32_real_array
         MODULE PROCEDURE func_integrate_1D_REAL64_real_array
@@ -19,6 +19,8 @@ MODULE mod_safe
         MODULE PROCEDURE func_integrate_2D_REAL128_real_array
 #endif
     END INTERFACE func_integrate_array
+
+    ! Declare interface ...
     INTERFACE func_interpolate_points
         MODULE PROCEDURE func_interpolate_REAL32_points
         MODULE PROCEDURE func_interpolate_REAL64_points
@@ -26,6 +28,8 @@ MODULE mod_safe
         MODULE PROCEDURE func_interpolate_REAL128_points
 #endif
     END INTERFACE func_interpolate_points
+
+    ! Declare interface ...
     INTERFACE func_overall_index
         MODULE PROCEDURE func_overall_index_2loops
         MODULE PROCEDURE func_overall_index_3loops
@@ -34,6 +38,8 @@ MODULE mod_safe
         MODULE PROCEDURE func_overall_index_6loops
         MODULE PROCEDURE func_overall_index_7loops
     END INTERFACE func_overall_index
+
+    ! Declare interface ...
     INTERFACE sub_allocate_array
         MODULE PROCEDURE sub_allocate_1D_INT8_integer_array
         MODULE PROCEDURE sub_allocate_2D_INT8_integer_array
@@ -115,6 +121,18 @@ MODULE mod_safe
         MODULE PROCEDURE sub_allocate_7D_REAL128_real_array
 #endif
     END INTERFACE sub_allocate_array
+
+    ! Declare interface ...
+    INTERFACE sub_flood_array
+        MODULE PROCEDURE sub_flood_REAL32_real_array
+    END INTERFACE sub_flood_array
+
+    ! Declare interface ...
+    INTERFACE sub_flood_array_iter
+        MODULE PROCEDURE sub_flood_REAL32_real_array_iter
+    END INTERFACE sub_flood_array_iter
+
+    ! Declare interface ...
     INTERFACE sub_load_array_from_BIN
         MODULE PROCEDURE sub_load_1D_INT8_integer_array_from_BIN
         MODULE PROCEDURE sub_load_2D_INT8_integer_array_from_BIN
@@ -196,6 +214,8 @@ MODULE mod_safe
         MODULE PROCEDURE sub_load_7D_REAL128_real_array_from_BIN
 #endif
     END INTERFACE sub_load_array_from_BIN
+
+    ! Declare interface ...
     INTERFACE sub_save_array_as_BIN
         MODULE PROCEDURE sub_save_1D_INT8_integer_array_as_BIN
         MODULE PROCEDURE sub_save_2D_INT8_integer_array_as_BIN
@@ -277,12 +297,16 @@ MODULE mod_safe
         MODULE PROCEDURE sub_save_7D_REAL128_real_array_as_BIN
 #endif
     END INTERFACE sub_save_array_as_BIN
+
+    ! Declare interface ...
     INTERFACE sub_save_array_as_PBM
         MODULE PROCEDURE sub_save_2D_INT8_logical_array_as_PBM
         MODULE PROCEDURE sub_save_2D_INT16_logical_array_as_PBM
         MODULE PROCEDURE sub_save_2D_INT32_logical_array_as_PBM
         MODULE PROCEDURE sub_save_2D_INT64_logical_array_as_PBM
     END INTERFACE sub_save_array_as_PBM
+
+    ! Declare interface ...
     INTERFACE sub_save_array_as_PGM
         MODULE PROCEDURE sub_save_2D_INT8_integer_array_as_PGM
         MODULE PROCEDURE sub_save_2D_INT16_integer_array_as_PGM
@@ -294,6 +318,8 @@ MODULE mod_safe
         MODULE PROCEDURE sub_save_2D_REAL128_real_array_as_PGM
 #endif
     END INTERFACE sub_save_array_as_PGM
+
+    ! Declare interface ...
     INTERFACE sub_save_array_as_PPM
         MODULE PROCEDURE sub_save_2D_INT8_integer_array_as_PPM
         MODULE PROCEDURE sub_save_2D_INT16_integer_array_as_PPM
@@ -305,6 +331,8 @@ MODULE mod_safe
         MODULE PROCEDURE sub_save_2D_REAL128_real_array_as_PPM
 #endif
     END INTERFACE sub_save_array_as_PPM
+
+    ! Declare interface ...
     INTERFACE sub_solve_quadratic_equation
         MODULE PROCEDURE sub_solve_quadratic_REAL32_equation
         MODULE PROCEDURE sub_solve_quadratic_REAL64_equation
@@ -446,6 +474,8 @@ MODULE mod_safe
     INCLUDE "mod_safe/sub_allocate_array/sub_allocate_6D_REAL128_real_array.f90"
     INCLUDE "mod_safe/sub_allocate_array/sub_allocate_7D_REAL128_real_array.f90"
 #endif
+    INCLUDE "mod_safe/sub_flood_array/sub_flood_REAL32_real_array.f90"
+    INCLUDE "mod_safe/sub_flood_array_iter/sub_flood_REAL32_real_array_iter.f90"
     INCLUDE "mod_safe/sub_load_array_from_BIN/sub_load_1D_INT8_integer_array_from_BIN.f90"
     INCLUDE "mod_safe/sub_load_array_from_BIN/sub_load_2D_INT8_integer_array_from_BIN.f90"
     INCLUDE "mod_safe/sub_load_array_from_BIN/sub_load_3D_INT8_integer_array_from_BIN.f90"
---
+++ mod_safe/sub_flood_array/sub_flood_REAL32_real_array.f90
@@ -0,0 +1,157 @@
+SUBROUTINE sub_flood_REAL32_real_array(nx, ny, elev, seaLevel, flooded, tileScale)
+    ! Import modules ...
+    USE ISO_FORTRAN_ENV
+
+    IMPLICIT NONE
+
+    ! Declare inputs/outputs ...
+    INTEGER(kind = INT64), INTENT(in)                                           :: nx
+    INTEGER(kind = INT64), INTENT(in)                                           :: ny
+    REAL(kind = REAL32), DIMENSION(nx, ny), INTENT(in)                          :: elev
+    REAL(kind = REAL32), INTENT(in)                                             :: seaLevel
+    LOGICAL(kind = INT8), DIMENSION(nx, ny), INTENT(inout)                      :: flooded
+    INTEGER(kind = INT64), INTENT(in)                                           :: tileScale
+
+    ! Declare parameters ...
+    INTEGER(kind = INT64), PARAMETER                                            :: nIters = 1000_INT64
+    INTEGER(kind = INT64), PARAMETER                                            :: nTileIters = 1000_INT64
+
+    ! Declare variables ...
+    INTEGER(kind = INT64)                                                       :: iIter
+    INTEGER(kind = INT64)                                                       :: iTileIter
+    INTEGER(kind = INT64)                                                       :: ix
+    INTEGER(kind = INT64)                                                       :: ixlo
+    INTEGER(kind = INT64)                                                       :: ixhi
+    INTEGER(kind = INT64)                                                       :: iy
+    INTEGER(kind = INT64)                                                       :: iylo
+    INTEGER(kind = INT64)                                                       :: iyhi
+    INTEGER(kind = INT64)                                                       :: newTot
+    INTEGER(kind = INT64)                                                       :: newTotTile
+    INTEGER(kind = INT64)                                                       :: oldTot
+    INTEGER(kind = INT64)                                                       :: oldTotTile
+
+    ! Check tileScale ...
+    IF(MOD(nx, tileScale) /= 0_INT64)THEN
+        WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"nx" is not an integer multiple of "tileScale"'
+        FLUSH(unit = ERROR_UNIT)
+        STOP
+    END IF
+    IF(MOD(ny, tileScale) /= 0_INT64)THEN
+        WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"ny" is not an integer multiple of "tileScale"'
+        FLUSH(unit = ERROR_UNIT)
+        STOP
+    END IF
+
+    ! Start ~infinite loop ...
+    DO iIter = 1_INT64, nIters
+        ! Find initial total flooded area ...
+        oldTot = COUNT(flooded, kind = INT64)
+
+        ! Stop looping once no more flooding can occur ...
+        IF(oldTot == 0_INT64 .OR. oldTot == nx * ny)THEN
+            EXIT
+        END IF
+
+        ! Flood the whole array once ...
+        CALL sub_flood_array_iter(                                              &
+                  nx = nx,                                                      &
+                  ny = ny,                                                      &
+                elev = elev,                                                    &
+            seaLevel = seaLevel,                                                &
+             flooded = flooded,                                                 &
+                ixlo = 1_INT64,                                                 &
+                ixhi = nx,                                                      &
+                iylo = 1_INT64,                                                 &
+                iyhi = ny                                                       &
+        )
+
+        !$omp parallel                                                          &
+        !$omp default(none)                                                     &
+        !$omp private(iTileIter)                                                &
+        !$omp private(ix)                                                       &
+        !$omp private(ixlo)                                                     &
+        !$omp private(ixhi)                                                     &
+        !$omp private(iy)                                                       &
+        !$omp private(iylo)                                                     &
+        !$omp private(iyhi)                                                     &
+        !$omp private(oldTotTile)                                               &
+        !$omp private(newTotTile)                                               &
+        !$omp shared(elev)                                                      &
+        !$omp shared(flooded)                                                   &
+        !$omp shared(nx)                                                        &
+        !$omp shared(ny)                                                        &
+        !$omp shared(seaLevel)                                                  &
+        !$omp shared(tileScale)
+            !$omp do                                                            &
+            !$omp schedule(dynamic)
+                ! Loop over x-axis tiles ...
+                DO ix = 1_INT64, nx / tileScale
+                    ! Find the extent of the tile ...
+                    ixlo = (ix - 1_INT64) * tileScale + 1_INT64
+                    ixhi =  ix            * tileScale
+
+                    ! Loop over y-axis tiles ...
+                    DO iy = 1_INT64, ny / tileScale
+                        ! Find the extent of the tile ...
+                        iylo = (iy - 1_INT64) * tileScale + 1_INT64
+                        iyhi =  iy            * tileScale
+
+                        ! Start ~infinite loop ...
+                        DO iTileIter = 1_INT64, nTileIters
+                            ! Find initial total flooded area ...
+                            oldTotTile = COUNT(flooded(ixlo:ixhi, iylo:iyhi), kind = INT64)
+
+                            ! Stop looping once no more flooding can occur ...
+                            IF(oldTotTile == 0_INT64 .OR. oldTotTile == tileScale ** 2)THEN
+                                EXIT
+                            END IF
+
+                            ! Flood a tile of the whole array once ...
+                            CALL sub_flood_array_iter(                          &
+                                      nx = nx,                                  &
+                                      ny = ny,                                  &
+                                    elev = elev,                                &
+                                seaLevel = seaLevel,                            &
+                                 flooded = flooded,                             &
+                                    ixlo = ixlo,                                &
+                                    ixhi = ixhi,                                &
+                                    iylo = iylo,                                &
+                                    iyhi = iyhi                                 &
+                            )
+
+                            ! Find new total flooded area ...
+                            newTotTile = COUNT(flooded(ixlo:ixhi, iylo:iyhi), kind = INT64)
+
+                            ! Stop looping once no more flooding has occured ...
+                            IF(newTotTile == oldTotTile)THEN
+                                EXIT
+                            END IF
+
+                            ! Cry ...
+                            IF(iTileIter == nTileIters)THEN
+                                WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) 'Failed to converge the flooding of a tile of the whole array'
+                                FLUSH(unit = ERROR_UNIT)
+                                STOP
+                            END IF
+                        END DO
+                    END DO
+                END DO
+            !$omp end do
+        !$omp end parallel
+
+        ! Find new total flooded area ...
+        newTot = COUNT(flooded, kind = INT64)
+
+        ! Stop looping once no more flooding has occured ...
+        IF(newTot == oldTot)THEN
+            EXIT
+        END IF
+
+        ! Cry ...
+        IF(iIter == nIters)THEN
+            WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) 'Failed to converge the flooding of the whole array'
+            FLUSH(unit = ERROR_UNIT)
+            STOP
+        END IF
+    END DO
+END SUBROUTINE sub_flood_REAL32_real_array

e3d6dd839ce9747eee92b8e35084ec67147b8adb
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2024-12-26 12:47:40 +0000

    make the flooding of the tiles optional

--- mod_safe/sub_flood_array/sub_flood_REAL32_real_array.f90
+++ mod_safe/sub_flood_array/sub_flood_REAL32_real_array.f90
@@ -65,79 +65,82 @@ SUBROUTINE sub_flood_REAL32_real_array(nx, ny, elev, seaLevel, flooded, tileScal
                 iyhi = ny                                                       &
         )
 
-        !$omp parallel                                                          &
-        !$omp default(none)                                                     &
-        !$omp private(iTileIter)                                                &
-        !$omp private(ix)                                                       &
-        !$omp private(ixlo)                                                     &
-        !$omp private(ixhi)                                                     &
-        !$omp private(iy)                                                       &
-        !$omp private(iylo)                                                     &
-        !$omp private(iyhi)                                                     &
-        !$omp private(oldTotTile)                                               &
-        !$omp private(newTotTile)                                               &
-        !$omp shared(elev)                                                      &
-        !$omp shared(flooded)                                                   &
-        !$omp shared(nx)                                                        &
-        !$omp shared(ny)                                                        &
-        !$omp shared(seaLevel)                                                  &
-        !$omp shared(tileScale)
-            !$omp do                                                            &
-            !$omp schedule(dynamic)
-                ! Loop over x-axis tiles ...
-                DO ix = 1_INT64, nx / tileScale
-                    ! Find the extent of the tile ...
-                    ixlo = (ix - 1_INT64) * tileScale + 1_INT64
-                    ixhi =  ix            * tileScale
-
-                    ! Loop over y-axis tiles ...
-                    DO iy = 1_INT64, ny / tileScale
+        ! Check that the tiles are large enough to bother with ...
+        IF(nx / tileScale >= 32_INT64 .AND. ny / tileScale >= 32_INT64)THEN
+            !$omp parallel                                                      &
+            !$omp default(none)                                                 &
+            !$omp private(iTileIter)                                            &
+            !$omp private(ix)                                                   &
+            !$omp private(ixlo)                                                 &
+            !$omp private(ixhi)                                                 &
+            !$omp private(iy)                                                   &
+            !$omp private(iylo)                                                 &
+            !$omp private(iyhi)                                                 &
+            !$omp private(oldTotTile)                                           &
+            !$omp private(newTotTile)                                           &
+            !$omp shared(elev)                                                  &
+            !$omp shared(flooded)                                               &
+            !$omp shared(nx)                                                    &
+            !$omp shared(ny)                                                    &
+            !$omp shared(seaLevel)                                              &
+            !$omp shared(tileScale)
+                !$omp do                                                        &
+                !$omp schedule(dynamic)
+                    ! Loop over x-axis tiles ...
+                    DO ix = 1_INT64, nx / tileScale
                         ! Find the extent of the tile ...
-                        iylo = (iy - 1_INT64) * tileScale + 1_INT64
-                        iyhi =  iy            * tileScale
+                        ixlo = (ix - 1_INT64) * tileScale + 1_INT64
+                        ixhi =  ix            * tileScale
 
-                        ! Start ~infinite loop ...
-                        DO iTileIter = 1_INT64, nTileIters
-                            ! Find initial total flooded area ...
-                            oldTotTile = COUNT(flooded(ixlo:ixhi, iylo:iyhi), kind = INT64)
+                        ! Loop over y-axis tiles ...
+                        DO iy = 1_INT64, ny / tileScale
+                            ! Find the extent of the tile ...
+                            iylo = (iy - 1_INT64) * tileScale + 1_INT64
+                            iyhi =  iy            * tileScale
 
-                            ! Stop looping once no more flooding can occur ...
-                            IF(oldTotTile == 0_INT64 .OR. oldTotTile == tileScale ** 2)THEN
-                                EXIT
-                            END IF
+                            ! Start ~infinite loop ...
+                            DO iTileIter = 1_INT64, nTileIters
+                                ! Find initial total flooded area ...
+                                oldTotTile = COUNT(flooded(ixlo:ixhi, iylo:iyhi), kind = INT64)
 
-                            ! Flood a tile of the whole array once ...
-                            CALL sub_flood_array_iter(                          &
-                                      nx = nx,                                  &
-                                      ny = ny,                                  &
-                                    elev = elev,                                &
-                                seaLevel = seaLevel,                            &
-                                 flooded = flooded,                             &
-                                    ixlo = ixlo,                                &
-                                    ixhi = ixhi,                                &
-                                    iylo = iylo,                                &
-                                    iyhi = iyhi                                 &
-                            )
+                                ! Stop looping once no more flooding can occur ...
+                                IF(oldTotTile == 0_INT64 .OR. oldTotTile == tileScale ** 2)THEN
+                                    EXIT
+                                END IF
 
-                            ! Find new total flooded area ...
-                            newTotTile = COUNT(flooded(ixlo:ixhi, iylo:iyhi), kind = INT64)
+                                ! Flood a tile of the whole array once ...
+                                CALL sub_flood_array_iter(                      &
+                                          nx = nx,                              &
+                                          ny = ny,                              &
+                                        elev = elev,                            &
+                                    seaLevel = seaLevel,                        &
+                                     flooded = flooded,                         &
+                                        ixlo = ixlo,                            &
+                                        ixhi = ixhi,                            &
+                                        iylo = iylo,                            &
+                                        iyhi = iyhi                             &
+                                )
 
-                            ! Stop looping once no more flooding has occured ...
-                            IF(newTotTile == oldTotTile)THEN
-                                EXIT
-                            END IF
+                                ! Find new total flooded area ...
+                                newTotTile = COUNT(flooded(ixlo:ixhi, iylo:iyhi), kind = INT64)
 
-                            ! Cry ...
-                            IF(iTileIter == nTileIters)THEN
-                                WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) 'Failed to converge the flooding of a tile of the whole array'
-                                FLUSH(unit = ERROR_UNIT)
-                                STOP
-                            END IF
+                                ! Stop looping once no more flooding has occured ...
+                                IF(newTotTile == oldTotTile)THEN
+                                    EXIT
+                                END IF
+
+                                ! Cry ...
+                                IF(iTileIter == nTileIters)THEN
+                                    WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) 'Failed to converge the flooding of a tile of the whole array'
+                                    FLUSH(unit = ERROR_UNIT)
+                                    STOP
+                                END IF
+                            END DO
                         END DO
                     END DO
-                END DO
-            !$omp end do
-        !$omp end parallel
+                !$omp end do
+            !$omp end parallel
+        END IF
 
         ! Find new total flooded area ...
         newTot = COUNT(flooded, kind = INT64)

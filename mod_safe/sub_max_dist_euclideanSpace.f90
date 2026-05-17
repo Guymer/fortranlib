64316c53c74f8811537d8e2a816996ff0c9a6a89
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2026-05-17 09:35:52 +0100

    add a subroutine to calculate the maximum distance to any location in a list of locations

---
+++ mod_safe/sub_max_dist_euclideanSpace.f90
@@ -0,0 +1,29 @@
+PURE SUBROUTINE sub_max_dist_euclideanSpace(n, lons, lats, midLon, midLat, maxDist)
+    ! Import standard modules ...
+    USE ISO_FORTRAN_ENV
+
+    IMPLICIT NONE
+
+    ! Declare input variables/outputs ...
+    INTEGER(kind = INT64), INTENT(in)                                           :: n
+    REAL(kind = REAL64), DIMENSION(n), INTENT(in)                               :: lons
+    REAL(kind = REAL64), DIMENSION(n), INTENT(in)                               :: lats
+    REAL(kind = REAL64), INTENT(in)                                             :: midLon
+    REAL(kind = REAL64), INTENT(in)                                             :: midLat
+    REAL(kind = REAL64), INTENT(out)                                            :: maxDist
+
+    ! Declare internal variables ...
+    INTEGER(kind = INT64)                                                       :: i
+
+    ! Find the maximum Euclidean distance from the middle to any location ...
+    maxDist = 0.0e0_REAL64                                                      ! [°]
+    DO i = 1_INT64, n
+        maxDist = MAX(                                                          &
+            maxDist,                                                            &
+            HYPOT(                                                              &
+                lons(i) - midLon,                                               &
+                lats(i) - midLat                                                &
+            )                                                                   &
+        )                                                                       ! [°]
+    END DO
+END SUBROUTINE sub_max_dist_euclideanSpace
